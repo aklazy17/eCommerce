1586c58f65e396775f0e7cc05600bb66ecc829aa
Amit Kumar <amitkumar.bz@outlook.com>
2025-01-24 17:12:12 +0530

    k8s scrpts

--- eCommerce.Product.API/Program.cs
+++ eCommerce.Product.API/Program.cs
@@ -81,11 +81,11 @@ builder.Services.AddOpenTelemetry()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 // Migrate db
 app.MigrateDatabase();
--- eCommerce.ProductDetail.API/Program.cs
+++ eCommerce.ProductDetail.API/Program.cs
@@ -55,11 +55,11 @@ builder.Services.AddOpenTelemetry()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 // Migrate db
 app.MigrateDatabase();
